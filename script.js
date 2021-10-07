3c616a33a0ed63b8e2e8d72c0a23b348ce2ed372
Yohan Gerbaud <86349498+Ygerbaud@users.noreply.github.com>
2021-10-07 11:33:49 +0200

    New page evenements

---
+++ script.js
@@ -0,0 +1,34 @@
+
+    const newP = document.createElement("p");
+    newP.classList = "copyDesk";
+    newP.innerHTML = "Nao_Web &copy; 2021";
+    const footer = document.getElementById("footer_top");
+    footer.appendChild(newP);   
+    var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
